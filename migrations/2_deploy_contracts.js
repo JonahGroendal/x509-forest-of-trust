8722e7a1bf0a47016b775ff9bc036c47501a6df8
Jonah Groendal <13501607+JonahGroendal@users.noreply.github.com>
2020-11-15 19:32:27 -0500

    Update truffle. Remove unnecessary deploy from migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-var Asn1Decode = artifacts.require("asn1-decode/Asn1Decode");
+// var Asn1Decode = artifacts.require("asn1-decode/Asn1Decode");
 var RsaSha256Algorithm = artifacts.require("sig-verify-algs/RsaSha256Algorithm");
 var X509ForestOfTrust = artifacts.require("X509ForestOfTrust");
 var ENSNamehash = artifacts.require("ens-namehash/ENSNamehash");
 var DateTime = artifacts.require("ethereum-datetime/DateTime");
 
 module.exports = function(deployer, network) {
-  deployer.deploy(Asn1Decode, { overwrite: false });
-  deployer.link(Asn1Decode, RsaSha256Algorithm);
-  deployer.link(Asn1Decode, X509ForestOfTrust);
+  // deployer.deploy(Asn1Decode, { overwrite: false });
+  // deployer.link(Asn1Decode, RsaSha256Algorithm);
+  // deployer.link(Asn1Decode, X509ForestOfTrust);
   deployer.deploy(ENSNamehash, { overwrite: false });
   deployer.link(ENSNamehash, X509ForestOfTrust);
   deployer.deploy(RsaSha256Algorithm, { overwrite: false })
